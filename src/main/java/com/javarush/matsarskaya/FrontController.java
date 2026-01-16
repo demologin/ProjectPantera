49b8bbee5ad49d8a48349be35b8a9b29cb29b718
KitaYaro-V <kitayaro15@gmail.com>
2026-01-16 12:24:51 +0300

    ADD FrontController + home.jsp

---
+++ src/main/java/com/javarush/matsarskaya/FrontController.java
@@ -0,0 +1,30 @@
+package com.javarush.matsarskaya;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class FrontController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Прямое перенаправление на JSP страницу
+        req.getRequestDispatcher("/WEB-INF/home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/home.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Добро пожаловать в игру "Путь всадника дракона"</h1>
+    <p>Здесь начинается ваше приключение!</p>
+</body>
+</html>
