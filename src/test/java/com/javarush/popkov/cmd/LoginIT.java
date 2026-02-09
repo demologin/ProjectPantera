b3ff67bf78be732dfd20735fe856a9d49bd3d995
Coderdecoder07 <coderdecoder07@gmail.com>
2026-02-09 17:20:00 +0200

    Step 2.6 - Added Jacoco and moved tests

---
+++ src/test/java/com/javarush/popkov/cmd/LoginIT.java
@@ -0,0 +1,42 @@
+package com.javarush.popkov.cmd;
+
+import com.javarush.popkov.BaseIT;
+import com.javarush.popkov.config.Winter;
+import com.javarush.popkov.entity.User;
+import com.javarush.popkov.service.UserService;
+import com.javarush.popkov.util.Key;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class LoginIT extends BaseIT {
+
+    final Login login = Winter.find(Login.class);
+    final UserService userService = Winter.find(UserService.class);
+
+    @Test
+    @DisplayName("When login admin then redirect to profile")
+    void whenLoginAdminThenRedirectToProfile() {
+        User user = userService.getAll().stream().findFirst().orElseThrow();
+        when(request.getParameter("login")).thenReturn(user.getLogin());
+        when(request.getParameter("password")).thenReturn(user.getPassword());
+
+        String actualRedirect = login.doPost(request);
+        Assertions.assertEquals("/profile", actualRedirect);
+
+        verify(session)
+                .setAttribute(eq(Key.USER), any(User.class));
+    }
+
+    @Test
+    @DisplayName("When incorrect login then redirect to login")
+    void whenIncorrectLoginThenRedirectToLogin() {
+        when(request.getParameter("login")).thenReturn("Carl");
+        when(request.getParameter("password")).thenReturn("err");
+
+        String actualRedirect = login.doPost(request);
+        Assertions.assertEquals("/login", actualRedirect);
+    }
+}
